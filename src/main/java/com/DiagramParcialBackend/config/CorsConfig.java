1e1af8c29fa5439328e7673472192cc948cfdb81
Mauricio Aleman <mauricio.aleman.3524@gmail.com>
2024-10-09 12:23:47 -0400

    .

---
+++ src/main/java/com/DiagramParcialBackend/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.DiagramParcialBackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://diagram-frontend-1er-parcial.vercel.app") // Permitir Koyeb
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Permitir credenciales si es necesario
+    }
+
+}
