c568a52902f5f522c8287a23f385af21e340cc9a
Mauricio Aleman <mauricio.aleman.3524@gmail.com>
2024-10-09 13:23:30 -0400

    .

--- src/main/java/com/DiagramParcialBackend/config/CorsConfig.java
+++ src/main/java/com/DiagramParcialBackend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://diagram-frontend-1er-parcial.vercel.app") // Origen específico
+                .allowedOrigins("*") // Origen específico
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*"); // Todos los encabezados permitidos
                 // Si usas credenciales
