7bd21d9df02243f5d68660f7be30d9d6870f9abc
Mauricio Aleman <mauricio.aleman.3524@gmail.com>
2024-10-09 02:58:21 -0400

    ..

--- src/main/java/com/DiagramParcialBackend/config/CorsConfig.java
+++ src/main/java/com/DiagramParcialBackend/config/CorsConfig.java
@@ -3,8 +3,9 @@ package com.DiagramParcialBackend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
+/*
+ *
+ *@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
@@ -17,3 +18,7 @@ public class CorsConfig implements WebMvcConfigurer {
     }
 
 }
+
+ *
+ *
+ * */
